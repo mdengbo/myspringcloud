e8221bd94efcb40f7290e4705b63cafa6275b5f4
你的用户名 <1440864716@qq.com>
2019-05-28 16:48:55 +0800

    springcloud  zuulfallback

---
+++ springcloud-gateway-zuul-fallback/src/main/java/com/example/zuul/fallbackconfig/myFallbackProvider.java
@@ -0,0 +1,66 @@
+package com.example.zuul.fallbackconfig;
+
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author madengbo
+ * @create 2019-05-28 13:44
+ * @desc 自定义所有fallback的 超类
+ * @Version 1.0
+ **/
+@Component
+public class myFallbackProvider implements ZuulFallbackProvider{
+
+
+    @Override
+    public String getRoute() {
+        return "springcloud-provider";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream(("fallback"+ getRoute()).getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
---
+++ springcloud-gateway-zuul-fallback/src/read.md
@@ -0,0 +1,4 @@
+# zuul fallback 与  hystrix ribbon fallback 区别
+    1、zuul fallback z注重是 调用微服务超时问题
+    2、hystirx 和 ribbon 做的是更加细粒度的控制   类或者 方法级别的超时控制
+    3、针对的粒度不同
