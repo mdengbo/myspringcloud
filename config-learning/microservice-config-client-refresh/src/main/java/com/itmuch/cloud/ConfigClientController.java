35ac00012376c7db1ae6df4b82e7576bdfeab4f3
你的用户名 <1440864716@qq.com>
2019-08-14 18:24:36 +0800

    springcloud config 更新

--- config-learning/microservice-config-client-refresh/src/main/java/com/itmuch/cloud/ConfigClientController.java
+++ config-learning/microservice-config-client-refresh/src/main/java/com/itmuch/cloud/ConfigClientController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+/**
+ * 因为 bean 实例是lazy  load 在第一初始化时  会进行缓存 当再次使用时 直接从缓存中获取
+ * 但是当 @RefreshScope 注解 该注解会监控对应的 configuration 的变化
+ * 假如有变  就会再次实例化 在进行缓存 避免再次由于某种需要 再次重启程序
+ *  需要在后台或者通过 postman 工具 post 请求：192.168.1.39:8081/refresh 以刷新缓存
+ *
+ * */
 @RefreshScope
 public class ConfigClientController {
 
